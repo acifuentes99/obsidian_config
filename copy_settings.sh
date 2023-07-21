71f2746fd69d5333c10cf2ce2419602d6cbb44dc
acifuentes99 <aecifuentesv@gmail.com>
2023-07-21 18:26:27 -0400

    setting copy script

---
+++ copy_settings.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+obsidian_config_folder="$1"
+
+
+cp .obsidian/obsidian_config_folder/plugins/quickadd/data.json $obsidian_config_folder/obsidian_config_folder/plugins/quickadd/data.json
+cp .obsidian/obsidian_config_folder/plugins/dataview/data.json $obsidian_config_folder/obsidian_config_folder/plugins/dataview/data.json
+cp .obsidian/obsidian_config_folder/plugins/periodic-notes/data.json $obsidian_config_folder/obsidian_config_folder/plugins/periodic-notes/data.json
+cp .obsidian/obsidian_config_folder/plugins/auto-note-mover/data.json $obsidian_config_folder/obsidian_config_folder/plugins/auto-note-mover/data.json
+cp .obsidian/obsidian_config_folder/plugins/templater-obsidian/data.json $obsidian_config_folder/obsidian_config_folder/plugins/templater-obsidian/data.json
+cp .obsidian/obsidian_config_folder/plugins/note-refactor-obsidian/data.json $obsidian_config_folder/obsidian_config_folder/plugins/note-refactor-obsidian/data.json
