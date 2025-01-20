3d904818339a9dba781350212a24a2449c6ed005
acifuentes99 <aecifuentesv@gmail.com>
2025-01-20 09:33:14 -0300

    Change Sync script for more cleaner code

--- version_obsidian_config.sh
+++ version_obsidian_config.sh
@@ -4,10 +4,7 @@ LOCAL_NOTES_FOLDER='/home/acifuentes/Documents/GoogleDrive/Notes'
 
 # Sync Meta folder contents
 rm -rf "$REPO_FOLDER/Z. Meta/.*"
-cp -r "$LOCAL_NOTES_FOLDER/Z. Meta/dv-scripts" "$REPO_FOLDER/Z. Meta/"
-cp -r "$LOCAL_NOTES_FOLDER/Z. Meta/js-scripts" "$REPO_FOLDER/Z. Meta/"
-cp -r "$LOCAL_NOTES_FOLDER/Z. Meta/custom-js" "$REPO_FOLDER/Z. Meta/"
-cp -r "$LOCAL_NOTES_FOLDER/Z. Meta/vimrc" "$REPO_FOLDER/Z. Meta/"
+cp -r "$LOCAL_NOTES_FOLDER/Z. Meta" "$REPO_FOLDER"
 
 rm -rf "$REPO_FOLDER/T. Templates/.*"
 cp -r "$LOCAL_NOTES_FOLDER/T. Templates" "$REPO_FOLDER"
